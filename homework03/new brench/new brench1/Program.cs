c825e499e573f5a69be71fa30fc52e8fcdedd299
KHumoyunmirzo <127532144+Humoyunmirzo-coder@users.noreply.github.com>
2023-03-28 23:16:54 +0500

    where code

---
+++ homework03/new brench/new brench1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace new_brench1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("mening birinch vazifam!");
+        }
+    }
+}
