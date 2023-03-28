29b11d1d17c83c28c34217ff294e57b9ba524756
KHumoyunmirzo <127532144+Humoyunmirzo-coder@users.noreply.github.com>
2023-03-28 23:34:54 +0500

    Update Program.cs

--- homework03/new brench/new brench1/Program.cs
+++ homework03/new brench/new brench1/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("mening birinch vazifam!");
+            Console.WriteLine("hello this is my one homework!");
+
+            Console.Write(" please namber type : ");
+            int namber = Console.Read();
+            int n1000 = namber / 1000;
+            int n100 = namber / 100;
+            int n10 = namber / 10;
+            int n1 = namber / 10%10;
+           int mac=   Math.Max(Math.Max(n1000, n100), Math.Max(n10, n1));
+            Console.WriteLine($" nambers maxsimal = {mac}");
+
         }
     }
 }
